28a8214b96d037957e7fa32f53d72628b76a0be7
Samuel Budge <17BudgeS@fulford.york.sch.uk>
2023-02-03 15:20:12 +0000

    lesson on 3/1/2021
    Now gives user headaches

--- server/Program.cs
+++ server/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace server
 {
@@ -14,40 +16,69 @@ namespace server
             Console.WriteLine("Welcome to the login thing");
             //create a http that listens on port 8080
             const int port = 8080;
-            string prefix = $"http://localoast:{port}";
+            string prefix = $"http://localhost:{port}/";
 
             Console.WriteLine($"Listening {prefix}");
             HttpListener server = new HttpListener();
             server.Prefixes.Add(prefix);
             server.Start();
-            HttpListenerContext context = server.GetContext();
+            
 
             
             bool running = true;
             int hitCount = 0;
             while (running)
             {
-            
+                HttpListenerContext context = server.GetContext();
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
             
-                string html = $"Ahhhhhhhhhhh you are request number {hitCount}".html;
-                hitCount++;
+                string html = "";
 
                 byte[] buffer = Encoding.UTF8.GetBytes(html);
 
                 Console.WriteLine($"Request  '{request.RawUrl}'");
 
+                if (request.HttpMethod == "POST")
+                {
+                    using(StreamReader r = new StreamReader(request.InputStream)){
+                        string query = r.ReadToEnd();
+                        Match m = Regex.match(query, "username=(.*)&password=(.*)");
+                        if(m.sucess)
+                        {
+                            string username = m.Groups[1].value;
+                            string username = m.Groups[2].value;
+
+                            string html = $"";
+
+                            if (username == "sam" && password == "password")
+                            {
+                                html = "login yes";
+                            } else {
+                                html = "login no";
+                            }
+
+
+                        }
+                    }
+
+                }
+
+                string html = $"";
+                byte[] buffer = Encoding.UTF8.GetBytes("");
+
                 switch(request.RawUrl)
                 {
                     case "/":
                         html = File.ReadAllText("../../static/index.html");
-                            break;
+                         buffer = Encoding.UTF8.GetBytes(html);
+                        response.ContentType = "text/html";
+                        break;
                     default:
                         string path = "../../static" + request.RawUrl;
                         if(File.Exists(path))
                         {
-                            html = File.ReadAllBytes(path);
+                            buffer = File.ReadAllBytes(path);
                         } else
                         {
                             response.StatusCode = 404;
@@ -60,7 +91,6 @@ namespace server
                         Console.WriteLine($"Unknown URL: {request.RawUrl}");
                         break;
                 }                                  
-}
 
                 Console.WriteLine($"Sending {buffer.Length} bytes");
                 response.ContentLength64 = buffer.Length;
